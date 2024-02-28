8f23f2a586055218c2ee906631c2246ee15e9849
Diego Mendoza <diegom.mendoza2601@gmail.com>
2024-02-27 21:30:56 -0400

    27-02-2024

---
+++ familia.pl
@@ -0,0 +1,41 @@
+# HECHOS
+cristiano.
+juan.
+batista.
+mario.
+adrian.
+luciano.
+alvaro.
+mariel.
+cristal.
+alejandra.
+
+esHombre(cristiano).
+esHombre(juan).
+esHombre(batista).
+esHombre(mario).
+esHombre(adrian).
+esHombre(luciano).
+esHombre(alvaro).
+
+esMujer(mariel).
+esMujer(cristal).
+esMujer(alejandra).
+
+es_hijo_o_hija(cristiano, juan).
+es_hijo_o_hija(cristiano, batista).
+es_hijo_o_hija(mariel, batista).
+es_hijo_o_hija(mariel, cristal).
+es_hijo_o_hija(batista, alejandra).
+es_hijo_o_hija(batista, adrian).
+es_hijo_o_hija(batista, luciano).
+es_hijo_o_hija(cristal, alejandra).
+es_hijo_o_hija(cristal, adrian).
+es_hijo_o_hija(cristal, luciano).
+es_hijo_o_hija(mario, alvaro).
+es_hijo_o_hija(alejandra, alvaro).
+
+
+
+
+
