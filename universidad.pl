6c8eab33b8f7bd85f8a74fd0a813f9a37a363dbb
Diego Mendoza <diegom.mendoza2601@gmail.com>
2024-03-08 21:03:35 -0400

    08-03-2024

---
+++ universidad.pl
@@ -0,0 +1,42 @@
+mario.
+arturica.
+andres.
+
+programacion1.
+redes2.
+inteligenciaArtificial.
+
+
+carla.
+magneta.
+giovana.
+elena.
+
+esHombre(mario).
+esHombre(andres).
+esMujer(arturica).
+esMujer(carla).
+esMujer(magneta).
+esMujer(giovana).
+esMujer(giovana).
+
+
+ensenia(mario, programacion1).
+ensenia(mario, redes2).
+
+ensenia(arturica, programacion1).
+ensenia(arturica, inteligenciaArtificial).
+
+ensenia(andres, redes2).
+
+asiste(carla, redes2).
+asiste(carla, inteligenciaArtificial).
+
+asiste(magneta, programacion1).
+asiste(magneta, inteligenciaArtificial).
+asiste(magneta, redes2).
+
+asiste(giovana, redes2).
+asiste(giovana, inteligenciaArtificial).
+
+asiste(elena, inteligenciaArtificial).
